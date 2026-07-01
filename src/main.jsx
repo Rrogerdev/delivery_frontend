2c5aaf08211a732ae78c9615e50b1c5cf3bfd743
RenanFerreirax <renanturmaensinomedio.com@gmail.com>
2026-07-01 11:46:42 -0300

    teste

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+import App from "./App";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
+
+<BrowserRouter basename="/20261prj5/delivery">
     <App />
   </BrowserRouter>
-)
+
+);
